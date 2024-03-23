f51ac240702d71c528fae4f644650d269c5ebae3
MD MAHFUZAR RAHMAN TAREK <mahfuzar175@gmail.com>
2024-03-24 00:36:40 +0600

    about section added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,19 +1,26 @@
 import { useEffect, useState } from "react";
 import { FaArrowRightLong } from "react-icons/fa6";
+import { FaInstagram } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
 const Home = () => {
-    const [portfolio, setPortfolio] = useState([]);
+  const [portfolio, setPortfolio] = useState([]);
 
-    useEffect(() => {
-        fetch('https://portfolio-backend-30mp.onrender.com/api/v1/get/user/65b3a22c01d900e96c4219ae')
-          .then((res) => {
-            return res.json();
-          })
-          .then((data) => {
-            console.log(data);
-            setPortfolio(data);
-          });
-      }, []);
-
+  useEffect(() => {
+    fetch(
+      "https://portfolio-backend-30mp.onrender.com/api/v1/get/user/65b3a22c01d900e96c4219ae"
+    )
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setPortfolio(data);
+      });
+  }, []);
 
   return (
     <div className="px-10 py-2 bg-black ">
@@ -102,27 +109,112 @@ const Home = () => {
       </div>
 
       {/* hero section */}
-      <section className="bg-black text-white lg:p-16 p-5">
-        <div className="flex flex-col lg:flex-row justify-between items-center lg:text-left text-center gap-8">
-            <div className="w-1/2 space-y-6">
-                <h1 className="font-libre font-semibold xl:text-8xl lg:text-6xl text-4xl">{portfolio.user?.about.title}</h1>
-                <div className="flex flex-col lg:flex-row justify-center items-center lg:gap-0 gap-5">
-                <a
-              href="#"
-              className="inline-flex items-center justify-center h-14 px-8 py-0 text-base font-semibold text-center text-white no-underline align-middle transition-all duration-300 ease-in-out bg-transparent  bg-gray-950 hover:border-2 border-gray-950 border-2 hover:bg-black border-solid  cursor-pointer select-none hover:text-white hover:border-white focus:shadow-xs focus:no-underline"
-            >
-              Contact
-              <FaArrowRightLong className="ml-2 mt-1" />
-            </a><p className="ml-5 xl:font-extrabold xl:text-base">{portfolio.user?.about.subTitle}</p>
-                </div>
+      <section className="bg-black text-white lg:p-16 p-5  py-24">
+        <div className="flex lg:flex-row justify-between items-center lg:text-left text-center gap-8 flex-col-reverse">
+          <div className="w-1/2 space-y-6">
+            <h1 className="font-libre font-semibold xl:text-8xl lg:text-6xl text-4xl">
+              {portfolio.user?.about.title}
+            </h1>
+            <div className="flex flex-col lg:flex-row justify-center items-center lg:gap-0 gap-5">
+              <a
+                href="#"
+                className="inline-flex items-center justify-center h-14 px-8 py-0 text-base font-semibold text-center text-white no-underline align-middle transition-all duration-300 ease-in-out bg-transparent  bg-gray-950 hover:border-2 border-gray-950 border-2 hover:bg-black border-solid  cursor-pointer select-none hover:text-white hover:border-white focus:shadow-xs focus:no-underline"
+              >
+                Contact
+                <FaArrowRightLong className="ml-2 mt-1" />
+              </a>
+              <p className="ml-5 xl:font-extrabold xl:text-base">
+                {portfolio.user?.about.subTitle}
+              </p>
             </div>
-            <div>
-                <img className="xl:w-96 lg:w-80 w-auto rounded-full" src={portfolio.user?.about.avatar.url} alt="" />
-            </div>
+          </div>
+          <div className="relative">
+            <img
+              className="xl:w-96 lg:w-80 w-auto rounded-full"
+              src={portfolio.user?.about.avatar.url}
+              alt=""
+            />
+            <h2 className="absolute bottom-24 xl:left-28 left-20 md:left-56 font-semibold text-4xl font-libre">
+              {portfolio.user?.about.name}
+            </h2>
+          </div>
         </div>
       </section>
 
+      <section className="bg-black text-white lg:p-16 p-5  py-24 border border-gray-900">
+        <Tabs>
+          <TabList className="text-center font-semibold text-lg font-libre mb-9">
+            <Tab>About Me</Tab>
+            <Tab>Skillset</Tab>
+          </TabList>
 
+          <TabPanel>
+            <div className="flex flex-col lg:flex-row justify-start gap-11">
+              <div className="lg:w-1/2">
+                <img
+                  className=""
+                  src={portfolio.user?.about.avatar.url}
+                  alt=""
+                />
+              </div>
+              <div className="space-y-9 lg:w-1/2">
+                <h1 className="font-semibold text-5xl font-libre">
+                  {portfolio.user?.about.subTitle}
+                </h1>
+                <p className="font-roboto text-gray-400">
+                  {portfolio.user?.about.description}
+                </p>
+                <div className="grid lg:grid-cols-2 gap-6">
+                  <div className="text-lg">
+                    <h2 className="font-roboto text-gray-400">Name</h2>
+                    <p className="font-libre">{portfolio.user?.about.name}</p>
+                  </div>
+                  <div className="text-lg">
+                    <h2 className="text-gray-400 font-roboto">Phone Number</h2>
+                    <p className="font-libre">
+                      {portfolio.user?.about.phoneNumber}
+                    </p>
+                  </div>
+                  <div className="text-lg">
+                    <h2 className="text-gray-400 font-roboto">Email Address</h2>
+                    <p className="font-libre">{portfolio.user?.email}</p>
+                  </div>
+                  <div className="text-lg">
+                    <h2 className="text-gray-400 font-roboto">
+                      Social Networks
+                    </h2>
+                    <div className="flex gap-4 font-libre  mt-1">
+                      <a href="#">
+                        <FaInstagram />
+                      </a>
+                      <a href="#">
+                        <FaLinkedin />
+                      </a>
+                      <a href="#">
+                        <FaTwitter />
+                      </a>
+                      <a href="#">
+                        <FaFacebook />
+                      </a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </TabPanel>
+          <TabPanel>
+            <div className="flex flex-col lg:flex-row justify-start gap-11">
+              <div className="space-y-8">
+                <h2 className="font-libre text-4xl">My Skills</h2>
+                <p className="font-roboto">{portfolio.user?.about.subTitle}</p>
+              </div>
+              <div>
+
+              </div>
+            </div>
+          </TabPanel>
+        </Tabs>
+      </section>
     </div>
   );
 };
