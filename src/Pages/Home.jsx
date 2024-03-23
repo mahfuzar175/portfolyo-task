9a56f38ba6b5e1105b681cc95b0225863444e529
MD MAHFUZAR RAHMAN TAREK <mahfuzar175@gmail.com>
2024-03-24 02:58:30 +0600

    reviews added

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -24,13 +24,13 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="px-10 py-2 bg-black ">
+    <div className=" py-2 bg-black ">
       {/* navbar section*/}
       <div className="drawer">
         <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
         <div className="drawer-content flex flex-col">
           {/* Navbar */}
-          <div className="w-full navbar bg-black text-white">
+          <div className="w-full navbar bg-black text-white px-10">
             <div className="flex-none lg:hidden">
               <label
                 htmlFor="my-drawer-3"
@@ -57,16 +57,16 @@ const Home = () => {
               <ul className="menu menu-horizontal font-roboto text-base gap-4">
                 {/* Navbar menu content here */}
                 <li>
-                  <a>Home</a>
+                  <a href="#">Home</a>
                 </li>
                 <li>
-                  <a>Services</a>
+                  <a href="#services">Services</a>
                 </li>
                 <li>
-                  <a>About</a>
+                  <a href="#about">About</a>
                 </li>
                 <li>
-                  <a>Project</a>
+                  <a href="#projects">Project</a>
                 </li>
                 <li>
                   <a>Review</a>
@@ -110,7 +110,7 @@ const Home = () => {
       </div>
 
       {/* hero section */}
-      <section className="bg-black text-white lg:p-16 p-5  py-24">
+      <section className="bg-black text-white lg:p-16 p-5  py-24 px-10">
         <div className="flex lg:flex-row justify-between items-center lg:text-left text-center gap-8 flex-col-reverse">
           <div className="w-1/2 space-y-6">
             <h1 className="font-libre font-semibold xl:text-8xl lg:text-6xl text-4xl">
@@ -143,142 +143,175 @@ const Home = () => {
       </section>
 
       {/* skill section */}
-      <section className="bg-black text-white lg:p-16 p-5  py-24 border border-gray-900">
-        <Tabs>
-          <TabList className="text-center font-semibold text-lg font-libre mb-9">
-            <Tab>About Me</Tab>
-            <Tab>Skillset</Tab>
-          </TabList>
+      <section className="bg-black text-white lg:p-16  p-5" id="about">
+        <div className="px-12 border border-gray-900 py-12">
+          <Tabs>
+            <TabList className="text-center font-semibold text-lg font-libre mb-9">
+              <Tab>About Me</Tab>
+              <Tab>Skillset</Tab>
+            </TabList>
 
-          <TabPanel>
-            <div className="flex flex-col lg:flex-row justify-start gap-11">
-              <div className="lg:w-1/2">
-                <img
-                  className=""
-                  src={portfolio.user?.about.avatar.url}
-                  alt=""
-                />
-              </div>
-              <div className="space-y-9 lg:w-1/2">
-                <h1 className="font-semibold text-5xl font-libre">
-                  {portfolio.user?.about.subTitle}
-                </h1>
-                <p className="font-roboto text-gray-400">
-                  {portfolio.user?.about.description}
-                </p>
-                <div className="grid lg:grid-cols-2 gap-6">
-                  <div className="text-lg">
-                    <h2 className="font-roboto text-gray-400">Name</h2>
-                    <p className="font-libre">{portfolio.user?.about.name}</p>
+            <TabPanel>
+              <div className="flex flex-col lg:flex-row justify-start gap-11">
+                <div className="lg:w-1/2">
+                  <img
+                    className=""
+                    src={portfolio.user?.about.avatar.url}
+                    alt=""
+                  />
+                </div>
+                <div className="space-y-9 lg:w-1/2">
+                  <h1 className="font-semibold text-5xl font-libre">
+                    {portfolio.user?.about.subTitle}
+                  </h1>
+                  <p className="font-roboto text-gray-400">
+                    {portfolio.user?.about.description}
+                  </p>
+                  <div className="grid lg:grid-cols-2 gap-6">
+                    <div className="text-lg">
+                      <h2 className="font-roboto text-gray-400">Name</h2>
+                      <p className="font-libre">{portfolio.user?.about.name}</p>
+                    </div>
+                    <div className="text-lg">
+                      <h2 className="text-gray-400 font-roboto">
+                        Phone Number
+                      </h2>
+                      <p className="font-libre">
+                        {portfolio.user?.about.phoneNumber}
+                      </p>
+                    </div>
+                    <div className="text-lg">
+                      <h2 className="text-gray-400 font-roboto">
+                        Email Address
+                      </h2>
+                      <p className="font-libre">{portfolio.user?.email}</p>
+                    </div>
+                    <div className="text-lg">
+                      <h2 className="text-gray-400 font-roboto">
+                        Social Networks
+                      </h2>
+                      <div className="flex gap-4 font-libre  mt-1">
+                        <a href="#">
+                          <FaInstagram />
+                        </a>
+                        <a href="#">
+                          <FaLinkedin />
+                        </a>
+                        <a href="#">
+                          <FaTwitter />
+                        </a>
+                        <a href="#">
+                          <FaFacebook />
+                        </a>
+                      </div>
+                    </div>
                   </div>
-                  <div className="text-lg">
-                    <h2 className="text-gray-400 font-roboto">Phone Number</h2>
-                    <p className="font-libre">
-                      {portfolio.user?.about.phoneNumber}
+                </div>
+              </div>
+            </TabPanel>
+            <TabPanel>
+              <div className="flex flex-col lg:flex-row justify-start gap-24">
+                <div className="space-y-8 lg:w-1/2">
+                  <div className="space-y-8">
+                    <h2 className="font-libre text-4xl">My Skills</h2>
+                    <p className="font-roboto">
+                      {portfolio.user?.about.subTitle}
                     </p>
                   </div>
-                  <div className="text-lg">
-                    <h2 className="text-gray-400 font-roboto">Email Address</h2>
-                    <p className="font-libre">{portfolio.user?.email}</p>
-                  </div>
-                  <div className="text-lg">
-                    <h2 className="text-gray-400 font-roboto">
-                      Social Networks
-                    </h2>
-                    <div className="flex gap-4 font-libre  mt-1">
-                      <a href="#">
-                        <FaInstagram />
-                      </a>
-                      <a href="#">
-                        <FaLinkedin />
-                      </a>
-                      <a href="#">
-                        <FaTwitter />
-                      </a>
-                      <a href="#">
-                        <FaFacebook />
-                      </a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </TabPanel>
-          <TabPanel>
-            <div className="flex flex-col lg:flex-row justify-start gap-24">
-              <div className="space-y-8 lg:w-1/2">
-                <div className="space-y-8">
-                  <h2 className="font-libre text-4xl">My Skills</h2>
-                  <p className="font-roboto">
-                    {portfolio.user?.about.subTitle}
-                  </p>
-                </div>
-                {/* progress */}
-                <div className="font-libre">
-                  {portfolio.user?.skills
-                    .sort((a, b) => a.sequence - b.sequence)
-                    .map((skill, index) => (
-                      <div key={index} className="mb-4">
-                        <div className="flex justify-between items-center">
-                          <h3>{skill.name}</h3>
-                          <p>{skill.percentage}%</p>
+                  {/* progress */}
+                  <div className="font-libre">
+                    {portfolio.user?.skills
+                      .sort((a, b) => a.sequence - b.sequence)
+                      .map((skill, index) => (
+                        <div key={index} className="mb-4">
+                          <div className="flex justify-between items-center">
+                            <h3>{skill.name}</h3>
+                            <p>{skill.percentage}%</p>
+                          </div>
+                          <progress
+                            className="progress w-full progress-success"
+                            value={skill.percentage}
+                            max="100"
+                          ></progress>
                         </div>
-                        <progress
-                          className="progress w-full progress-success"
-                          value={skill.percentage}
-                          max="100"
-                        ></progress>
+                      ))}
+                  </div>
+                </div>
+
+                <div>
+                  <div className="grid grid-cols-2 lg:grid-cols-3 gap-3">
+                    {portfolio.user?.skills.map((skill, index) => (
+                      <div key={index}>
+                        <img
+                          className="w-40 bg-gray-800 rounded-lg"
+                          src={skill.image.url}
+                          alt=""
+                        />
                       </div>
                     ))}
+                  </div>
                 </div>
               </div>
-
-              <div>
-                <div className="grid grid-cols-2 lg:grid-cols-3 gap-3">
-                  {portfolio.user?.skills.map((skill, index) => (
-                    <div key={index}>
-                      <img
-                        className="w-40 bg-gray-800 rounded-lg"
-                        src={skill.image.url}
-                        alt=""
-                      />
-                    </div>
-                  ))}
-                </div>
-              </div>
-            </div>
-          </TabPanel>
-        </Tabs>
+            </TabPanel>
+          </Tabs>
+        </div>
       </section>
 
-        {/* projects section */}
-        <section className="bg-black text-white lg:p-16 p-5 py-24 mt-9">
-  <h2 className="font-libre font-semibold text-4xl mb-8">My Recent Works</h2>
-  <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-    {portfolio.user?.projects.sort((a, b) => a.sequence - b.sequence).map((project, index) => (
-      <div key={index} className="relative">
-        <img
-          src={project.image.url}
-          alt=""
-          className="w-full h-auto  filter brightness-100 hover:brightness-75"
-        />
-        <div className="absolute inset-0 duration-500 flex items-center justify-center opacity-0 transition bg-gray-800 h-[82%]  hover:opacity-75">
-          
-          <a
-                href="#"
-                className="inline-flex items-center justify-center h-14 px-8 py-0 text-base font-semibold text-center text-white no-underline align-middle transition-all duration-300 ease-in-out hover:bg-transparent  hover:border-2 bg-black border-black border-2  border-solid  cursor-pointer select-none hover:text-white hover:border-white focus:shadow-xs focus:no-underline"
-              >
-                View Code
-                <FaGithub className="ml-2 mt-1"/>
-              </a>
-        </div>
-        <h2 className="text-center font-libre text-3xl mt-4">Project {project.sequence}</h2>
+      {/* projects section */}
+      <div className="border-b-gray-900 border-b" id="projects">
+        <section className="bg-black text-white lg:p-16 p-5 py-26 mb-10">
+          <h2 className="font-libre font-semibold text-4xl mb-8">
+            My Recent Works
+          </h2>
+          <div className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+            {portfolio.user?.projects
+              .sort((a, b) => a.sequence - b.sequence)
+              .map((project, index) => (
+                <div key={index} className="relative">
+                  <img
+                    src={project.image.url}
+                    alt=""
+                    className="w-full h-auto  filter brightness-100 hover:brightness-75"
+                  />
+                  <div className="absolute inset-0 duration-500 flex items-center justify-center opacity-0 transition bg-gray-800 h-[82%]  hover:opacity-75">
+                    <a
+                      href="#"
+                      className="inline-flex items-center justify-center h-14 px-8 py-0 text-base font-semibold text-center text-white no-underline align-middle transition-all duration-300 ease-in-out hover:bg-transparent  hover:border-2 bg-black border-black border-2  border-solid  cursor-pointer select-none hover:text-white hover:border-white focus:shadow-xs focus:no-underline"
+                    >
+                      View Code
+                      <FaGithub className="ml-2 mt-1" />
+                    </a>
+                  </div>
+                  <div className="relative text-center font-libre text-3xl mt-4  cursor-pointer transition-all ease-in-out before:transition-[width] before:ease-in-out before:duration-700 before:absolute before:bg-white before:origin-center before:h-[1px] before:w-0 hover:before:w-[50%] before:bottom-0 before:left-[50%] after:transition-[width] after:ease-in-out after:duration-700 after:absolute after:bg-white after:origin-center after:h-[1px] after:w-0 hover:after:w-[50%] after:bottom-0 after:right-[50%]">
+                    <span className="">Project {project.sequence}</span>
+                  </div>
+                </div>
+              ))}
+          </div>
+        </section>
       </div>
-    ))}
-  </div>
-</section>
 
+      {/* review section */}
+      <section
+        className="bg-black text-white lg:p-16 p-5  py-24 px-10"
+        id="reviews"
+      >
+        <div className="mt-12">
+        {portfolio.user?.testimonials.map((testimonial, index) => (
+          <div key={index} className="mb-4 mt-12">
+            <h2 className="font-libre font-semibold lg:text-4xl text-xl">" {testimonial.review} "</h2>
+            <div className="flex justify-start items-center mt-6">
+            <img className="hidden lg:block" src={testimonial.image.url} alt="" />
+            <div>
+            <h2 className="font-libre">{testimonial.name}</h2>
+            <p className="text-slate-300 font-roboto">{testimonial.position}</p>
+            </div>
+            </div>
 
+          </div>
+        ))}
+        </div>
+      </section>
     </div>
   );
 };
